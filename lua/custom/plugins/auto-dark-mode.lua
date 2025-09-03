1fd7324f5b1a20a222dbc061291a43631e1f7cd1
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2025-09-03 18:43:48 +0200

    chore: Change theme

--- lua/custom/plugins/auto-dark-mode.lua
+++ lua/custom/plugins/auto-dark-mode.lua
@@ -3,16 +3,10 @@ return {
     'f-person/auto-dark-mode.nvim',
     opts = {
       set_dark_mode = function()
-        require('visual_studio_code').setup {
-          mode = 'dark',
-        }
-        vim.cmd.colorscheme 'visual_studio_code'
+        require('material.functions').change_style 'darker'
       end,
       set_light_mode = function()
-        require('visual_studio_code').setup {
-          mode = 'light',
-        }
-        vim.cmd.colorscheme 'visual_studio_code'
+        require('material.functions').change_style 'lighter'
       end,
       update_interval = 100,
       fallback = 'light',
