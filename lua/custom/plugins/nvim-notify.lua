ffb61448ccf4cbc8053c25ef437819e5169c6e68
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2025-09-05 19:19:01 +0200

    chore: Reformat

--- lua/custom/plugins/nvim-notify.lua
+++ lua/custom/plugins/nvim-notify.lua
@@ -2,7 +2,8 @@ return {
   'rcarriga/nvim-notify',
   config = function()
     require('notify').setup {
-    render = 'compact',
+      merge_duplicates = true,
+      render = 'compact',
       stages = 'fade',
     }
   end,
