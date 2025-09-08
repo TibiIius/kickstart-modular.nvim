630294cea975898c5b88e840c70a99a033240bea
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2025-09-08 09:58:29 +0200

    feat: Setup DAP correctly
    * Fix DAP-UI
    * Setup keybinds
    * Enable better parsing of launch.json files by using json5

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -1,10 +1,33 @@
 return {
   {
     'mfussenegger/nvim-dap',
+    dependencies = {
+      'Joakker/lua-json5',
+    },
     config = function()
       local dap = require 'dap'
 
+      -- launch.json files can have comments etc, so we need json5
+      require('dap.ext.vscode').json_decode = require'json5'.parse
+
+      vim.keymap.set('n', '<leader>dt', dap.terminate, { desc = '[D]AP [T]erminate' })
+      vim.keymap.set('n', '<leader>dn', ':DapNew<CR>', { desc = '[D]AP [N]ew' })
+      vim.keymap.set('n', '<leader>dso', ':DapStepOver<CR>', { desc = '[D]AP [S]tep [O]ver' })
+      vim.keymap.set('n', '<leader>dso', ':DapStepOut<CR>', { desc = '[D]AP [S]tep O[u]t' })
+      vim.keymap.set('n', '<leader>dsi', ':DapStepInto<CR>', { desc = '[D]AP [S]tep [I]nto' })
+      vim.keymap.set('n', '<leader>dc', ':DapStepInto<CR>', { desc = '[D]AP [C]ontinue' })
+      vim.keymap.set('n', '<leader>db', ':DapToggleBreakpoint<CR>', { desc = '[D]AP Toggle [B]reakpoint' })
+      vim.keymap.set('n', '<leader>dB', ':DapClearBreakpoints<CR>', { desc = '[D]AP Clear [B]reakpoints' })
+
       dap.adapters.codelldb = {
+        type = 'executable',
+        command = 'codelldb',
+
+        -- On windows you may have to uncomment this:
+        -- detached = false,
+      }
+
+      dap.adapters.lldb = {
         type = 'executable',
         command = 'codelldb',
 
@@ -20,9 +43,7 @@ return {
       local dap = require 'dap'
       local dapui = require 'dapui'
 
-      local map = function(keys, func, desc, mode)
-        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'DAP: ' .. desc })
-      end
+      dapui.setup()
 
       vim.keymap.set('n', '<leader>dr', dapui.toggle, { desc = '[D]AP [R]eveal' })
 
