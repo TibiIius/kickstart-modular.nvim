06aac05abde02c63b1b2a17ad67cf1ee4cafa680
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2025-09-17 12:16:05 +0200

    feat: Configure python
    * Add venv selector
    * Configure lsp correctly

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -184,7 +184,16 @@ return {
       local servers = {
         clangd = {},
         gopls = {},
-        pyright = {},
+        pyright = {
+          -- Using Ruff's import organizer
+          disableOrganizeImports = true,
+        },
+        python = {
+          analysis = {
+            -- Ignore all files for analysis to exclusively use Ruff for linting
+            ignore = { '*' },
+          },
+        },
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
