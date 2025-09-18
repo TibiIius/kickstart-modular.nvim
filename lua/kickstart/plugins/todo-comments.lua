4877b34682b096a97e2ec5f6bc0ca35948c85305
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2025-09-18 17:19:05 +0200

    feat: Snacks integration for TODO comments

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,29 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'folke/snacks.nvim',
+    },
+    opts = { signs = false },
+    keys = {
+      {
+        '<leader>st',
+        function()
+          Snacks.picker.todo_comments()
+        end,
+        desc = '[S]earch All [T]ODO',
+      },
+      {
+        '<leader>sT',
+        function()
+          Snacks.picker.todo_comments { keywords = { 'TODO', 'FIX', 'FIXME', 'HACK', 'PERF', 'WARN', 'WARNING' } }
+        end,
+        desc = '[S]earch [T]ODO (only TODO, FIX, FIXME, HACK, PERF, WARN, WARNING)',
+      },
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
