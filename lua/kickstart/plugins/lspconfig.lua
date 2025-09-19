ddacccef34b8fa6db28064a78fdfc3a01f77a12d
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2025-09-19 12:25:01 +0200

    chore: Don't install formatters with lspconfig
    * We have mason-conform for that

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -244,10 +244,6 @@ return {
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-        'prettier',
-      })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       for server, config in pairs(servers) do
