b8f346641afd55d1729a96123384100cd253c9fc
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2025-12-04 20:06:37 +0100

    feat: Add Copilot chat

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,1 +1,15 @@
-return { 'github/copilot.vim' }
+return {
+  {
+    'github/copilot.vim',
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken',
+    opts = {
+      ui_select = true,
+    },
+  },
+}
