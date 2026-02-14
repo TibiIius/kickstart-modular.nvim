0f5d63122d7e2324a19c8c4ecf93160784e83bbe
Tim Friedrich Brüggemann <oss@timfb.dev>
2026-02-14 21:51:32 +0100

    chore: More sensible defaults for rust-analyzer

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -208,7 +208,22 @@ return {
             ignore = { '*' },
           },
         },
-        rust_analyzer = {},
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              files = {
+                excludeDirs = {
+                  ".flatpak",
+                  ".flatpak-builder",
+                  ".repo",
+                  "build",
+                  "_build",
+                  "workdir"
+                }
+              }
+            }
+          }
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
