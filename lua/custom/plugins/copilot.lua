9a4eff350ff73a7142c778654ebe2168b16aaa7c
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2026-03-13 11:10:59 +0100

    chore: Remove Copilot chat

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -2,14 +2,4 @@ return {
   {
     'github/copilot.vim',
   },
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    dependencies = {
-      { 'nvim-lua/plenary.nvim', branch = 'master' },
-    },
-    build = 'make tiktoken',
-    opts = {
-      ui_select = true,
-    },
-  },
 }
