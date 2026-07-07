6512b62cd0a624cc15e26a7c1d2cb7f36c00f257
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2026-07-07 15:19:10 +0200

    chore: Replace deprecated treesitter plugin

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -5,15 +5,10 @@ local function gh(repo) return 'https://github.com/' .. repo end
 --
 --  See `:help nvim-treesitter-intro`
 
-vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter' } }
+vim.pack.add { { src = gh 'romus204/tree-sitter-manager.nvim' } }
 
-local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
-
-require('nvim-treesitter').setup {
-  ensure_installed = parsers,
+require('tree-sitter-manager').setup {
   auto_install = true,
-  highlight = { enable = true },
-  indent = { enable = true },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
